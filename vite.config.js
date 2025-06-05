f4915389b65845a4340b662a14ab2e072bd95061
den-2107 <stalkerden69@gmail.com>
2025-06-05 18:53:16 +0300

    Тестирование авторазвёртки 9

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build'
+    outDir: 'build', // важно для GitHub Actions: билд будет идти в /build
+    emptyOutDir: true // очищает старые файлы перед сборкой
   }
 })
